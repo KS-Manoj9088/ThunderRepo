2b07603a84cff5671d7e0755afd8736bed955700
KS MANOJ KUMAR <ksmk2104@gmail.com>
2024-09-14 11:57:17 +0530

    Deleted patterns 6,7,8,9,10

--- pattern6.py
+++
@@ -1,5 +0,0 @@
-#  To  print square pattern with alphabet symbols
-
-n  = int(input("Enter no of rows : "))
-for i in range(n):
-    print((chr(65+i)+" ")*n)
--- pattern7.py
+++
@@ -1,7 +0,0 @@
-# to print square pattern with digits
-
-n = int(input("Enter no of rows : "))
-for i in range(n):
-    for j in range(n):
-        print(str(j+1),end=" ")
-    print()
