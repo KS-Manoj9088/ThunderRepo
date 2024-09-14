233d156ba8b24482e00be8edf1889e8d829d96ef
KS MANOJ KUMAR <ksmk2104@gmail.com>
2024-09-14 10:32:21 +0530

    Created a new repo on patterns and Added pattern - 1,2

---
+++ patterns/pattern1.py
@@ -0,0 +1,11 @@
+#This is 1st pattern we are going to draw.
+#we are going to draw a straight line pattern using stars.
+
+n = int(input("Enter no of stars : "))
+for i in range(n):
+    print("*",end=" ")
+    
+    
+    
+#Expected output
+#Enter no of stars : 5 -> * * * * *
